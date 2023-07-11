05ddd676d7cc7e767c3587e440f44b7b572f4cf8
Arne Binder <arne.binder@dfki.de>
2023-07-11 22:28:29 +0200

    further simplify convert_doc_to_conllua_lines

--- src/metrics/coref.py
+++ src/metrics/coref.py
@@ -1,10 +1,10 @@
 import logging
-from typing import Any, Dict, List
+from typing import Any, List
 
 import pandas as pd
-from pytorch_ie.core import Document
 
 from src.metrics.interface import DocumentMetric
+from src.taskmodules.coref_hoi_preprocessed import Conll2012OntonotesV5PreprocessedDocument
 from src.utils.coval.eval.evaluator import (
     b_cubed,
     blancc,
@@ -21,71 +21,41 @@ from src.utils.coval.ua.reader import get_coref_infos
 logger = logging.getLogger(__name__)
 
 
-def get_clusters(
-    mention_predictions: List[dict], cluster_predictions: List[dict]
-) -> List[List[List[int]]]:
-    mention_ids = {}
-    for mention in mention_predictions:
-        mention_ids[mention["_id"]] = [mention["start"], mention["end"] - 1]
-    clusters = []
-    for el in cluster_predictions:
-        cluster_mentions = []
-        for span_id in el["spans"]:
-            cluster_mentions.append(mention_ids[span_id])
-        clusters.append(cluster_mentions)
+def convert_doc_to_conllua_lines(
+    document: Conll2012OntonotesV5PreprocessedDocument, use_predictions: bool
+) -> List[str]:
 
-    return clusters
-
-
-def convert_doc_to_conllua_lines(document: Document, use_predictions: bool) -> List[str]:
-    predictions_or_annotations = "predictions" if use_predictions else "annotations"
-    orig_doc = document.asdict()
-    clusters = get_clusters(
-        orig_doc["mentions"][predictions_or_annotations],
-        orig_doc["clusters"][predictions_or_annotations],
-    )
-    doc_as_dict = {
-        "doc_key": orig_doc["id"],
-        "tokens": orig_doc["tokens"],
-        "sentences": orig_doc["sentences"],
-        "speakers": [],
-        "constituents": [],
-        "ner": [],
-        "clusters": clusters,
-        "sentence_map": orig_doc["sentence_map"],
-        "subtoken_map": orig_doc["subtoken_map"],
-        "pronouns": [],
-    }
-
-    pred_clusters = [tuple(tuple(m) for m in cluster) for cluster in doc_as_dict["clusters"]]
+    cluster_annotations = document.clusters.predictions if use_predictions else document.clusters
 
     lines = []
     lines.append(
         "# global.columns = ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC IDENTITY BRIDGING DISCOURSE_DEIXIS REFERENCE NOM_SEM"
     )
-    lines.append("# newdoc id = " + doc_as_dict["doc_key"])
+    lines.append("# newdoc id = " + str(document.id))
     markable_id = 1
     entity_id = 1
 
-    coref_strs = [""] * len(doc_as_dict["tokens"])
+    coref_strs = [""] * len(document.tokens)
+    if document.subtoken_map is None:
+        raise ValueError("subtoken_map is None")
 
-    for clus in pred_clusters:
-        for (start, end) in clus:
-            start = doc_as_dict["subtoken_map"][start]
-            end = doc_as_dict["subtoken_map"][end]
+    for clus in cluster_annotations:
+        for m in clus.spans:
+            subtoken_start = document.subtoken_map[m.start]
+            subtoken_end = document.subtoken_map[m.end - 1]
 
-            coref_strs[start] += "(EntityID={}|MarkableID=markable_{}".format(
+            coref_strs[subtoken_start] += "(EntityID={}|MarkableID=markable_{}".format(
                 entity_id, markable_id
             )
             markable_id += 1
-            if start == end:
-                coref_strs[end] += ")"
+            if subtoken_start == subtoken_end:
+                coref_strs[subtoken_end] += ")"
             else:
-                coref_strs[end] = ")" + coref_strs[end]
+                coref_strs[subtoken_end] = ")" + coref_strs[subtoken_end]
 
         entity_id += 1
 
-    for _id, token in enumerate(doc_as_dict["tokens"]):
+    for _id, token in enumerate(document.tokens):
         if coref_strs[_id] == "":
             coref_strs[_id] = "_"
         sentence = "{}  {}  _  _  _  _  _  _  _  _  {}  _  _  _  _".format(
@@ -135,7 +105,7 @@ class CorefMetrics(DocumentMetric):
         self.gold_lines = []
         self.sys_lines = []
 
-    def _update(self, document: Document) -> None:
+    def _update(self, document: Conll2012OntonotesV5PreprocessedDocument) -> None:
         self.gold_lines.extend(convert_doc_to_conllua_lines(document, use_predictions=False))
         self.sys_lines.extend(convert_doc_to_conllua_lines(document, use_predictions=True))
 
