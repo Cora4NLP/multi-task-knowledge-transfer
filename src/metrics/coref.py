e0886e4abf08710488c1464e3bd9f8b689b1b5e0
tanikina <tanikina@users.noreply.github.com>
2023-07-12 12:26:06 +0200

    Coreference evaluation (#17)
    * update evaluation files
    
    * export conllua serializer
    
    * manually add changes introduced by the pre-commit checks
    
    * update pre-commit config, codespell fix
    
    * implement config based CorefMetrics
    
    * merge convert_json_to_ua and convert_doc_to_ua_lines
    
    * further simplify convert_doc_to_conllua_lines
    
    * cleanup
    
    * throw exception if document has wrong type
    
    * fix exception
    
    * remove unused files
    
    * remove some remnants
    
    * name source of the original code and minor cleanup
    
    * fix merge issue
    
    ---------
    
    Co-authored-by: tatianak <36244-tatianak@users.noreply.gitlab.aicrowd.com>
    Co-authored-by: tanikina <tatiana.anikina@dfki.de>
    Co-authored-by: Ubuntu <ubuntu@dfki.lxd>
    Co-authored-by: Arne Binder <arne.binder@dfki.de>

---
+++ src/metrics/coref.py
@@ -0,0 +1,144 @@
+import logging
+from typing import Any, List
+
+import pandas as pd
+from pytorch_ie.core import Document
+
+from src.metrics.interface import DocumentMetric
+from src.taskmodules.coref_hoi_preprocessed import Conll2012OntonotesV5PreprocessedDocument
+from src.utils.coval.eval.evaluator import (
+    b_cubed,
+    blancc,
+    blancn,
+    ceafe,
+    ceafm,
+    evaluate_documents,
+    evaluate_non_referrings,
+    lea,
+    muc,
+)
+from src.utils.coval.ua.reader import get_coref_infos
+
+AVAILABLE_METRICS = {
+    "lea": lea,
+    "muc": muc,
+    "bcub": b_cubed,
+    "ceafe": ceafe,
+    "ceafm": ceafm,
+    "blanc": [blancc, blancn],
+}
+
+logger = logging.getLogger(__name__)
+
+
+def convert_doc_to_conllua_lines(document: Document, use_predictions: bool) -> List[str]:
+    if not isinstance(document, Conll2012OntonotesV5PreprocessedDocument):
+        raise TypeError(
+            f"document must be of type Conll2012OntonotesV5PreprocessedDocument to convert to conllua, "
+            f"but it is of type {type(document)}"
+        )
+
+    cluster_annotations = document.clusters.predictions if use_predictions else document.clusters
+
+    lines = []
+    lines.append(
+        "# global.columns = ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC IDENTITY BRIDGING DISCOURSE_DEIXIS REFERENCE NOM_SEM"
+    )
+    lines.append("# newdoc id = " + str(document.id))
+    markable_id = 1
+    entity_id = 1
+
+    coref_strs = [""] * len(document.tokens)
+    if document.subtoken_map is None:
+        raise ValueError("subtoken_map is None")
+
+    for clus in cluster_annotations:
+        for m in clus.spans:
+            subtoken_start = document.subtoken_map[m.start]
+            subtoken_end = document.subtoken_map[m.end - 1]
+
+            coref_strs[subtoken_start] += "(EntityID={}|MarkableID=markable_{}".format(
+                entity_id, markable_id
+            )
+            markable_id += 1
+            if subtoken_start == subtoken_end:
+                coref_strs[subtoken_end] += ")"
+            else:
+                coref_strs[subtoken_end] = ")" + coref_strs[subtoken_end]
+
+        entity_id += 1
+
+    for _id, token in enumerate(document.tokens):
+        if coref_strs[_id] == "":
+            coref_strs[_id] = "_"
+        sentence = "{}  {}  _  _  _  _  _  _  _  _  {}  _  _  _  _".format(
+            _id, token, coref_strs[_id]
+        )
+        lines.append(sentence)
+
+    return lines
+
+
+class CorefMetrics(DocumentMetric):
+    def __init__(
+        self,
+        keep_singletons: bool = True,
+        keep_split_antecedent: bool = True,
+        use_MIN: bool = False,
+        keep_non_referring: bool = False,
+        keep_bridging: bool = False,
+        only_split_antecedent: bool = False,
+        evaluate_discourse_deixis: bool = False,
+        show_as_markdown: bool = False,
+        metrics: List[str] = ["lea", "muc", "bcub", "ceafe", "ceafm", "blanc"],
+    ):
+        self.keep_singletons = keep_singletons
+        self.keep_split_antecedent = keep_split_antecedent
+        self.use_MIN = use_MIN
+        self.keep_non_referring = keep_non_referring
+        self.keep_bridging = keep_bridging
+        self.only_split_antecedent = only_split_antecedent
+        self.evaluate_discourse_deixis = evaluate_discourse_deixis
+
+        self.show_as_markdown = show_as_markdown
+
+        self.metrics = {k: AVAILABLE_METRICS[k] for k in metrics}
+
+        self.reset()
+
+    def reset(self):
+        self.gold_lines = []
+        self.sys_lines = []
+
+    def _update(self, document: Document) -> None:
+        self.gold_lines.extend(convert_doc_to_conllua_lines(document, use_predictions=False))
+        self.sys_lines.extend(convert_doc_to_conllua_lines(document, use_predictions=True))
+
+    def _values(self) -> Any:
+
+        doc_coref_infos, doc_non_referring_infos, doc_bridging_infos = get_coref_infos(
+            self.gold_lines,
+            self.sys_lines,
+            self.keep_singletons,
+            self.keep_split_antecedent,
+            self.keep_bridging,
+            self.keep_non_referring,
+            self.evaluate_discourse_deixis,
+            self.use_MIN,
+            print_debug=True,
+        )
+
+        metrics_dict = {}
+        for name, metric in self.metrics.items():
+            recall, precision, f1 = evaluate_documents(
+                doc_coref_infos, metric, beta=1, only_split_antecedent=self.only_split_antecedent
+            )
+            metrics_dict[name] = {
+                "precision": round(precision, 3),
+                "recall": round(recall, 3),
+                "f1": round(f1, 3),
+            }
+        if self.show_as_markdown:
+            logger.info(f"evaluation:\n{pd.DataFrame(metrics_dict).T.to_markdown()}")
+
+        return metrics_dict
---
+++ src/utils/ua_scorer.py
@@ -0,0 +1,77 @@
+from src.utils.coval.eval import evaluator
+from src.utils.coval.eval.evaluator import evaluate_non_referrings
+from src.utils.coval.ua import reader
+
+
+# based on: https://github.com/juntaoy/universal-anaphora-scorer/blob/main/ua-scorer.py
+def eval_coref_ua_scorer(coref_gold, coref_sys):
+    metric_dict = {
+        "lea": evaluator.lea,
+        "muc": evaluator.muc,
+        "bcub": evaluator.b_cubed,
+        "ceafe": evaluator.ceafe,
+        "ceafm": evaluator.ceafm,
+        "blanc": [evaluator.blancc, evaluator.blancn],
+    }
+    metrics = [(k, metric_dict[k]) for k in metric_dict]
+    # TA: pass these as parameters (too many?)
+    # or create a new config for coreference evaluation
+    keep_singletons = True
+    keep_split_antecedent = True
+    use_MIN = False
+    keep_non_referring = False
+    keep_bridging = False
+    only_split_antecedent = False
+    evaluate_discourse_deixis = False
+
+    metrics_out = evaluate(
+        coref_gold,
+        coref_sys,
+        metrics,
+        keep_singletons,
+        keep_split_antecedent,
+        keep_bridging,
+        keep_non_referring,
+        only_split_antecedent,
+        evaluate_discourse_deixis,
+        use_MIN,
+    )
+    return metrics_out
+
+
+def evaluate(
+    key_file,
+    sys_file,
+    metrics,
+    keep_singletons,
+    keep_split_antecedent,
+    keep_bridging,
+    keep_non_referring,
+    only_split_antecedent,
+    evaluate_discourse_deixis,
+    use_MIN,
+):
+
+    doc_coref_infos, doc_non_referring_infos, doc_bridging_infos = reader.get_coref_infos(
+        key_file,
+        sys_file,
+        keep_singletons,
+        keep_split_antecedent,
+        keep_bridging,
+        keep_non_referring,
+        evaluate_discourse_deixis,
+        use_MIN,
+        print_debug=True,
+    )
+
+    metrics_dict = {}
+    for name, metric in metrics:
+        recall, precision, f1 = evaluator.evaluate_documents(
+            doc_coref_infos, metric, beta=1, only_split_antecedent=only_split_antecedent
+        )
+        metrics_dict[name] = {
+            "precision": round(precision, 3),
+            "recall": round(recall, 3),
+            "f1": round(f1, 3),
+        }
+    return metrics_dict
