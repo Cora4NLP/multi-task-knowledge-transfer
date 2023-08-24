bbe046ef260e259eb92d4225560c63c69d07068d
Arne Binder <arne.binder@dfki.de>
2023-08-24 19:30:56 +0200

    fully implement AttentionBasedAggregator

--- src/models/components/transformer_multi_model.py
+++ src/models/components/transformer_multi_model.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, List, Optional
+from copy import copy
+from typing import Any, Dict, List, Optional, Union
 
 import torch
 from torch.nn import Module, ModuleDict
@@ -28,21 +29,61 @@ def aggregate_sum(x: Dict[str, torch.Tensor]) -> torch.Tensor:
     return aggregated
 
 
-class SelfAttentionBasedAggregator(Module):
-    def __init__(self, input_size: int, n_models: int):
+class AttentionBasedAggregator(Module):
+    def __init__(
+        self,
+        input_size: int,
+        n_models: int,
+        hidden_size: int = 128,
+        query_idx: Union[int, str] = 0,
+    ):
         super().__init__()
-        self.linear = torch.nn.Linear(input_size, n_models)
+        # the index of the model to use as the query. If a string is provided, it is the key of the model in
+        # the input dictionary. If an int is provided, it is the index of the model *values* in the input dictionary.
+        self.query_idx = query_idx
+        self.hidden_size = hidden_size
+        self.n_models = n_models
+        # we need only one query (just for the target model embeddings)
+        self.query = torch.nn.Linear(input_size, hidden_size)
+        # we need keys and values for all model embeddings
+        self.keys = torch.nn.ModuleList(
+            [torch.nn.Linear(input_size, hidden_size) for _ in range(n_models)]
+        )
+        self.values = torch.nn.ModuleList(
+            [torch.nn.Linear(input_size, input_size) for _ in range(n_models)]
+        )
 
     def forward(self, x: Dict[str, torch.Tensor]) -> torch.Tensor:
-        # (batch_size, num_tokens, hidden_size, num_models)
-        stacked = torch.stack(list(x.values()), dim=-1)
+        query_key = (
+            self.query_idx if isinstance(self.query_idx, str) else list(x.keys())[self.query_idx]
+        )
+
+        # (batch_size, num_tokens, hidden_size)
+        query = self.query(x[query_key])
         # (batch_size, num_tokens, num_models, num_models)
-        scores = self.linear(stacked.transpose(-1, -2))
-        # TODO: is dim=-1 correct here? Or should it be dim=-2?
-        attention = torch.softmax(scores, dim=-1)
-        # (batch_size, num_tokens, hidden_size, num_models)
-        weighted = torch.matmul(stacked, attention)
+        keys = torch.stack([k(v) for k, v in zip(self.keys, x.values())], dim=-1)
+        # (batch_size, num_tokens, num_models, output_size)
+        values = torch.stack([k(v) for k, v in zip(self.values, x.values())], dim=-1)
+
+        # flatten (combine batch dim and token dim) to calculate attention weights
+        # (batch_size x num_tokens, 1, hidden_size)
+        query_flat = query.view(-1, 1, self.hidden_size)
+        # (batch_size x num_tokens, hidden_size, num_models)
+        keys_flat = keys.view(-1, self.hidden_size, self.n_models)
+        # (batch_size x num_tokens, 1, num_models)
+        scores_flat = torch.bmm(query_flat, keys_flat)
+        attention_flat = torch.softmax(scores_flat, dim=-1)
+
+        # unflatten
+        batch_size, num_tokens = query.shape[:2]
+        # (batch_size, num_tokens, 1, num_models)
+        attention = attention_flat.view(batch_size, num_tokens, 1, self.n_models)
+
+        # (batch_size, num_tokens, output_size, num_models)
+        weighted = values * attention
+        # (batch_size, num_tokens, output_size)
         aggregated = weighted.sum(dim=-1)
+
         return aggregated
 
 
@@ -56,7 +97,12 @@ class TransformerMultiModel(Module):
         # if False, do not load the weights of the pretrained_models. Useful to save bandwidth when the model
         # is already trained because we load the weights from the checkpoint after initialisation.
         load_model_weights: bool = True,
-        aggregate: str = "mean",
+        # can be either a string indicating an aggregation type or a dictionary with a "type" key and additional
+        # parameters for the aggregation function. Currently, the following aggregation types are supported:
+        # - "mean": average the logits of all models
+        # - "sum": sum the logits of all models
+        # - "attention": use an attention mechanism to aggregate the logits of all models
+        aggregate: Union[str, Dict[str, Any]] = "mean",
         # A list of model ids to freeze during training.
         freeze_models: Optional[List[str]] = None,
         # A dictionary of config overrides to pass to AutoConfig.from_pretrained.
@@ -87,17 +133,26 @@ class TransformerMultiModel(Module):
         for model_id in freeze_models or []:
             self.models[model_id].requires_grad_(False)
 
-        if aggregate == "mean":
+        aggregate_config: Dict[str, Any]
+        if isinstance(aggregate, str):
+            aggregate_config = {"type": aggregate}
+        else:
+            # copy to not modify the original
+            aggregate_config = copy(aggregate)
+        aggregate_type = aggregate_config.pop("type")
+        if aggregate_type == "mean":
             self.aggregate = aggregate_mean
-        elif aggregate == "sum":
+        elif aggregate_type == "sum":
             self.aggregate = aggregate_sum
         elif aggregate == "concat":
             self.aggregate = ConcatAggregator(
                 input_size=self.config.hidden_size, num_models=len(self.models)
             )
-        elif aggregate == "self_attention":
-            self.aggregate = SelfAttentionBasedAggregator(
-                input_size=self.config.hidden_size, n_models=len(self.models)
+        elif aggregate_type == "attention":
+            self.aggregate = AttentionBasedAggregator(
+                input_size=self.config.hidden_size,
+                n_models=len(self.models),
+                **aggregate_config,
             )
         else:
             raise NotImplementedError(f"Aggregate method '{aggregate}' is not implemented")
