7f937ac4e37dc5d66025a5aca86fdf005cc0baa8
Arne Binder <arne.binder@dfki.de>
2023-10-12 15:31:13 +0200

    add stub for F1ForExtractiveQuestionAnswering

--- src/metrics/f1.py
+++ src/metrics/f1.py
@@ -1,8 +1,11 @@
 import logging
+from collections import defaultdict
 from typing import Callable, Optional, Tuple
 
 from pytorch_ie.core import Annotation, Document
 from pytorch_ie.metrics import F1Metric
+
+from src.taskmodules.extractive_question_answering import ExtractiveAnswer, ExtractiveQADocument
 
 logger = logging.getLogger(__name__)
 
@@ -57,3 +60,55 @@ class F1BestVsCandidatesMetric(F1Metric):
         else:
             # tp, fp, fn
             return 0, 1, 1
+
+
+class F1ForExtractiveQuestionAnswering(F1Metric):
+    def __init__(self, score_field: str = "score", **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.score_field = score_field
+
+    def calculate_counts(
+        self,
+        document: ExtractiveQADocument,
+        annotation_filter: Optional[Callable[[Annotation], bool]] = None,
+    ) -> Tuple[int, int, int]:
+        if annotation_filter is not None:
+            raise ValueError(f"{self.__class__.__name__} does not support annotation filters.")
+        predicted_annotations_per_question = defaultdict(set)
+        ann: ExtractiveAnswer
+        for ann in document[self.layer].predictions:
+            predicted_annotations_per_question[ann.question].add(ann)
+        gold_annotations_per_question = defaultdict(set)
+        for ann in document[self.layer]:
+            gold_annotations_per_question[ann.question].add(ann)
+
+        tp, fp, fn = 0, 0, 0
+        for question in set(gold_annotations_per_question) | set(
+            predicted_annotations_per_question
+        ):
+            gold_annotations = gold_annotations_per_question.get(question, set())
+            predicted_annotations = predicted_annotations_per_question.get(question, set())
+
+            if len(gold_annotations) == 0 and len(predicted_annotations) == 0:
+                continue
+
+            if len(gold_annotations) > 0 and len(predicted_annotations) == 0:
+                fn += 1
+                continue
+
+            if len(gold_annotations) == 0 and len(predicted_annotations) > 0:
+                fp += 1
+                continue
+
+            if any(getattr(ann, self.score_field) is None for ann in predicted_annotations):
+                raise ValueError(
+                    f"All predicted annotations must have a {self.score_field} value to calculate "
+                    f"{self.__class__.__name__}."
+                )
+            best_predicted_annotation = max(
+                predicted_annotations, key=lambda ann: getattr(ann, self.score_field)
+            )
+
+            # TODO: calculate the actual tp, fp, fn with respect to all gold questions
+
+        return tp, fp, fn
